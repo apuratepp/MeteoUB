925d33ebc87f230c01a19ffb385e64247ac11e5f
Josep <apuratepp@gmail.com>
2011-12-26 13:29:13 +0100

    afegint documentació

--- dades.rb
+++ dades.rb
@@ -3,12 +3,4 @@ require 'lib/meteo_ub.rb'
 
 meteo = MeteoUB.new
 meteo.parse :file => "tmp/www.dat"
-
-# puts meteo.datetime
-# p meteo.dades
-puts meteo.datetime
-puts meteo.sunrise
-puts meteo.sunset
-puts meteo.temperature
-puts meteo.humidity
-puts meteo.pressure
+p meteo.dades
--- lib/meteo_ub.rb
+++ lib/meteo_ub.rb
@@ -1,3 +1,7 @@
+# MeteoUB
+# Tractament de les dades meteorològiques del Departament d'Astronomia i Meteorologia 
+# de la Universitat de Barcelona
+
 require 'date'
 
 class MeteoUB
@@ -11,21 +15,29 @@ class MeteoUB
   attr_accessor :plou
   attr_accessor :dades
   
+  # Inicialització de l'array de les dades de l'arxiu
   def initialize
     @dades_raw = []
   end
   
+  # Mètode per llegir les dades de l'arxiu en el cas que aquest existeixi
   def parse(params)
     if File.exists?(params[:file])
       file = open(params[:file])
       file.each_line { |line| @dades_raw.push(line) }
       @datetime    = DateTime.strptime(self.dades_raw[0].chomp + " " + self.dades_raw[1].chomp + " UTC", "%d-%m-%y %k:%M %Z")
-      @temperature = self.dades_raw[2].chomp.to_f
-      @pressure    = self.dades_raw[8].chomp.to_f
-      @humidity    = self.dades_raw[5].chomp.to_f
+      @temperature = self.dades_raw[4].chomp.to_f
+      @pressure    = self.dades_raw[10].chomp.to_f
+      @humidity    = self.dades_raw[7].chomp.to_f
       @plou        = self.dades_raw[22].chomp == 1
-      @sunrise     = DateTime.strptime(self.dades_raw[0].chomp + " " + self.dades_raw[19].chomp + " UTC", "%d-%m-%y %k:%M %Z")
-      @sunset      = DateTime.strptime(self.dades_raw[0].chomp + " " + self.dades_raw[20].chomp + " UTC", "%d-%m-%y %k:%M %Z")
+      
+      # A vegades el minut el l'arxiu és '60'. S'ha de tenir en compte!
+      begin
+        @sunrise     = DateTime.strptime(self.dades_raw[0].chomp + " " + self.dades_raw[19].chomp + " UTC", "%d-%m-%y %k:%M %Z")
+        @sunset      = DateTime.strptime(self.dades_raw[0].chomp + " " + self.dades_raw[20].chomp + " UTC", "%d-%m-%y %k:%M %Z")
+      rescue
+        puts "Problema amb les dates de @sunrise o @sunset"
+      end
     
       @dades = {
         :datetime => @datetime,
