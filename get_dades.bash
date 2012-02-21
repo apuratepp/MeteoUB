2fc6540070a8123fa7dba3b99ac0ff3964b3bb36
Josep <apuratep@Joseps-MacBook-Air.local>
2012-02-21 23:24:57 +0100

    alguns canvis des de l'Air

--- get_dades.bash
+++ get_dades.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Script exemple per baixar les dades meteorològiques al directori adequat
 
-wget "http://infomet.am.ub.es/campbell/www.dat" -O "./tmp/www.dat"
-wget "http://infomet.am.ub.edu/campbell/maxmin.dat" -O "./tmp/maxmin.dat"
+wget "http://infomet.am.ub.es/campbell/www.dat" 	-O "./tmp/www.dat"
+wget "http://infomet.am.ub.edu/campbell/maxmin.dat" 	-O "./tmp/maxmin.dat"
+wget "http://infomet.am.ub.es/metdata/plu.dat" 		-O "./tmp/plu.dat"
