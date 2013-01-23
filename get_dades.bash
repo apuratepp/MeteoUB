80c77b09076dc43338e9fd9e4f631040aecc4116
Josep <apuratepp@gmail.com>
2013-01-23 23:54:00 +0100

    modificat get dades

--- get_dades.bash
+++ get_dades.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script exemple per baixar les dades meteorològiques al directori adequat
 
-curl "http://infomet.am.ub.es/campbell/www.dat" > "./tmp/www.dat"
-curl "http://infomet.am.ub.edu/campbell/maxmin.dat" > "./tmp/maxmin.dat"
-curl "http://infomet.am.ub.es/metdata/plu.dat" > "./tmp/plu.dat"
+curl "http://infomet.am.ub.es/campbell/www.dat" > "/home/apuratep/sites/MeteoUB/tmp/www.dat"
+curl "http://infomet.am.ub.es/campbell/www.dat" > "/home/apuratep/sites/secretaria/tmp/dades.dat"
+curl "http://infomet.am.ub.edu/campbell/maxmin.dat" > "/home/apuratep/sites/MeteoUB/tmp/maxmin.dat"
+curl "http://infomet.am.ub.es/metdata/plu.dat" > "/home/apuratep/sites/MeteoUB/tmp/plu.dat"
